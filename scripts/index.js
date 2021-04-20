81b18b9ec093b77682566585a1451549635935c9
kanhayaKy <kaku18is@cmrit.ac.in>
2021-04-21 01:20:22 +0530

    Added the resume

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
-var ul = document.querySelector("ul")
-function openNav(){
-    ul.classList.toggle("open")
+var ul = document.querySelector("ul");
+function openNav() {
+  ul.classList.toggle("open");
 }
+
+let navLinks = document.querySelectorAll(".nav-link");
+
+const activateLink = (event) => {
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+  event.target.classList.add("active");
+};
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", activateLink);
+  });
